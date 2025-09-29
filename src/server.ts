3a37574f18fea0f3aec49f6f29b9b4704bc44c0a
JOEL FRANCIS <joelfrancis9398@gmail.com>
2025-09-29 12:26:55 +0530

    chore :env bug fixed

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,22 @@
+
 import express from "express"
 import cors, { CorsOptions } from "cors";
 import router from "./routes/ocrRoutes";
+
+const app = express()
 import dotenv from "dotenv";
-
 dotenv.config();
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-
+const PORT = process.env.PORT || 3000
 const corsOptions: CorsOptions = {
-  origin: process.env.FRONTEND_URL,  
+  origin: ["process.env.FRONTEND_URL","http://localhost:7000","https://ocrclient.vercel.app/"],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
-
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
 app.use("/api", router);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-  console.log(`Frontend URL allowed: ${process.env.FRONTEND_URL}`);
-});
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
