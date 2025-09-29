364f80821efd902374ba7adba1d4a49d1e1574f1
JOEL FRANCIS <joelfrancis9398@gmail.com>
2025-09-29 11:55:05 +0530

    chore :env bug fixed

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,26 @@
-
 import express from "express"
 import cors, { CorsOptions } from "cors";
 import router from "./routes/ocrRoutes";
-
-const app = express()
 import dotenv from "dotenv";
+
 dotenv.config();
-const PORT = process.env.PORT || 3000
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
 const corsOptions: CorsOptions = {
-  origin: ["FRONTEND_URL","http://localhost:7000"],
+  origin: process.env.FRONTEND_URL,  
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
-app.use(cors(corsOptions))
+
+app.use(cors(corsOptions));
 
 app.use("/api", router);
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Frontend URL allowed: ${process.env.FRONTEND_URL}`);
+});
