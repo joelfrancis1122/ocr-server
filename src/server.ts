581e126453d86d81fe11699916f76497bf8fc045
JOEL FRANCIS <joelfrancis9398@gmail.com>
2025-09-29 13:28:08 +0530

    test :env bug test

--- src/server.ts
+++ src/server.ts
@@ -10,17 +10,18 @@ const PORT = process.env.PORT || 3000;
 
 // CORS options for both dev and prod
 const corsOptions: CorsOptions = {
-  origin: [
-    process.env.FRONTEND_URL_PROD || "https://ocrclient.vercel.app", 
-    process.env.FRONTEND_URL_DEV || "http://localhost:5173"          
-  ],
+  // origin: [
+  //   process.env.FRONTEND_URL_PROD || "https://ocrclient.vercel.app", 
+  //   process.env.FRONTEND_URL_DEV || "http://localhost:5173"          
+  // ],
+  origin:"*",
   credentials: true,
   methods: ["GET","POST","PUT","DELETE","OPTIONS","PATCH"],
   allowedHeaders: ["Content-Type","Authorization"],
 };
 
 app.use(cors(corsOptions));
-app.use(express.json()); // make sure JSON body parsing works
+app.use(express.json()); 
 app.use("/api", router);
 
 app.listen(PORT, () => {
