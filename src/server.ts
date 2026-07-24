694495bb2c3957bbae3702ba5bb1c1f3f47b77d1
joelfrancis1122 <joelfrancis9398@gmail.com>
2026-07-24 15:24:57 +0530

    refactor(server): clean up server entry point and CORS setup

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import cors, { CorsOptions } from "cors";
+import cors from "cors";
 import router from "./routes/ocrRoutes";
 import dotenv from "dotenv";
 
@@ -8,9 +8,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors()); 
-
-app.use(express.json()); 
+app.use(cors());
+app.use(express.json());
 app.use("/api", router);
 
 app.listen(PORT, () => {
